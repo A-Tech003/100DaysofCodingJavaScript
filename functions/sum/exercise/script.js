5337ec4a6734495258842b6b0d82877bc2aa633c
cleverprogrammer <>
2023-10-27 18:41:28 +0100

    just finished my first function task

--- functions/sum/exercise/script.js
+++ functions/sum/exercise/script.js
@@ -8,23 +8,24 @@
   ES6 Syntax (Arrow function): const add = () => {}
 */
 
-function add(){
-  //Add function here
+const add = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const div = (a, b) => {
+  if(b == 0){
+    return "undefined"
+  }else{
+    return a/b;
+  }
 }
 
-function sub(){
-  //Subtract function here
-}
-
-function div(){
-  //Divide function here
-}
-
-function mul(){
-  //Multiply function here
-}
+const mul = (a, b) => a * b;
 
 console.log('hello from the SUM exercise')
 /* 
   TODO: create a function that console logs the result of any of the above operations.
 */
+
+console.log(add(4, 4));
+console.log(div(2, 2));
